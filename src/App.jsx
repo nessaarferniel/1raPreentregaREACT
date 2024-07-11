679b05779e439e8d4466a2ba24a7f225f0826184
Nessaarferniel <nessaarferniel@gmail.com>
2024-07-11 18:32:49 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./style.css";
+import Navbar from './components/navbar/NavBar.jsx';
+import Greeting from './components/main/Greeting.jsx';
+export default function App() {
+  return (
+    <>
+      <Navbar />
+      <Greeting titulo1="Bienvenido a Foodier " titulo2="Un Mundo de recetas y sabores.." />
+    </>
+  );
+}
+
